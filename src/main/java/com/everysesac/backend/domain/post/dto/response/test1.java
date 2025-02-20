781eb7cb4858b89fc8c4f9430a5d6b895a549d91
novembertwotwo <korea621214@naver.com>
2025-02-20 11:03:36 +0900

    [BE][Feat]: PageRequestDTO,PageResponseDTO 추가

--- src/main/java/com/everysesac/backend/domain/post/dto/response/test1.java
+++
@@ -1,4 +0,0 @@
-package com.everysesac.backend.domain.post.dto.response;
-
-public class test1 {
-}
--- src/main/java/com/everysesac/backend/domain/post/repository/PostQueryRepository.java
+++ src/main/java/com/everysesac/backend/domain/post/repository/PostQueryRepository.java
@@ -10,6 +10,4 @@ import org.springframework.stereotype.Repository;
 public class PostQueryRepository {
     private final JPAQueryFactory queryFactory;
 
-
-
 }
