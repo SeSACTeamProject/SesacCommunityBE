64186c6144ed057d4b1e74807ece14baa953db4b
uhyunjin <uhyeonjin0619@gmail.com>
2025-02-24 14:08:03 +0900

    [BE][Feat]:Insert 테스트 완료

--- src/main/java/com/everysesac/backend/domain/post/controller/PostController.java
+++ src/main/java/com/everysesac/backend/domain/post/controller/PostController.java
@@ -1,19 +1,27 @@
 package com.everysesac.backend.domain.post.controller;
 
 import com.everysesac.backend.domain.post.dto.PostDTO;
+import com.everysesac.backend.domain.post.entity.Post;
+import com.everysesac.backend.domain.post.service.PostService;
+import com.everysesac.backend.global.dto.ApiResponse;
+import com.everysesac.backend.global.exception.ErrorResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
-import lombok.Getter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/posts")
 public class PostController {
+    @Autowired
+    PostService postService;
 
     @Tag(name="test api", description = "it's a test controller method")
     @Operation(summary = "post insert", description = "new post insertion")
@@ -22,11 +30,14 @@ public class PostController {
         @Parameter(name="postType", description = "study or team")
 //    @ApiResponse(responseCode = "201", description = "insert successful")
     @PostMapping("/register")
-    public ResponseEntity<String> register(@Valid PostDTO postDTO) {
-        try {
-            return ResponseEntity.status(HttpStatus.CREATED).body("게시글이 성공적으로 작성되었습니다.");
-        } catch (Exception e){
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Insert failed : " + e.getMessage());
-        }
+    public ResponseEntity<ApiResponse<PostDTO>> register(@Valid PostDTO postDTO) {
+        PostDTO postResponse = postService.register(postDTO);
+        ApiResponse<PostDTO> apiResponse = new ApiResponse<>(
+                HttpStatus.CREATED.value()+"",
+                201,
+                "post created"
+        );
+        apiResponse.setDto(postResponse);
+        return ResponseEntity.status(HttpStatus.CREATED).body(apiResponse);
     }
 }
